afd3c1eab8df380eb811a96040b7b36c9c32ec26
Allen Wu <allen.wu@ucla.edu>
2014-04-05 23:02:41 -0700

    Initial Commit

---
+++ GitGutter/AWGitGutter.h
@@ -0,0 +1,13 @@
+//
+//  AWGitGutter.h
+//  AWGitGutter
+//
+//  Created by Allen Wu on 4/5/14.
+//  Copyright (c) 2014 Allen Wu. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface AWGitGutter : NSObject
+
+@end
